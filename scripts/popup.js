9a09e580fbee4b811a382594b934af294f752b10
Salvatore Randazzo <salvatorerandazzo@Administrators-MacBook-Pro.local>
2019-01-30 17:09:59 -0500

    Added styling and project selection element

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,15 +1,11 @@
-let changeColor = document.getElementById('changeColor');
+let publish = document.getElementById('publish');
+let submit =  document.getElementById('submit');
 
-chrome.storage.sync.get('color', function(data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
-});
 
-changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            {code: 'document.body.style.backgroundColor = "' + color + '";'});
-    });
+publish.onclick = function(element) {
+    alert('publish');
+};
+
+submit.onclick = function() {
+    alert('submit');
 };
